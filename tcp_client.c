1121c0eb3768e3050b010aa02fa10ad14d38fb82
kmays170 <jmarch170@gmail.com>
2018-10-06 20:31:59 -0400

    Update tcp_client.c

--- tcp_client.c
+++ tcp_client.c
@@ -11,14 +11,14 @@ int main(){
 	network_socket = socket(AF_INET, SOCK_STREAM, 0);
 
 	struct sockaddr_in server_address;
-	serveraddress.sin_family = AF_INET;
+	server_address.sin_family = AF_INET;
 	server_address.sin_port = htons(9002);
 	server_address.sin_addr.s_addr = INADDR_ANY;
 	
 	int connection_status = connect(network_socket, (struct sockaddr *) &server_address, sizeof(server_address));
 	//checking for connection error
 	if (connection_status == -1){
-		printf("There was an error creating the connection \n\n"):
+		printf("There was an error creating the connection \n\n");
 	}
 	char server_response[256];
 	recv(network_socket, &server_response, sizeof(server_response), 0);
